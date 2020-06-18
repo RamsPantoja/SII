9a680094540f1294c1fe4a2ffbdedde108b42af8
Ramiro Angel Pantoja Cordoba <worddraco1@gmail.com>
2020-06-18 00:19:10 -0500

    Se formatearon los estilos del componente StudentDrawer

--- client/src/components/student_components/drawer/student_drawer.js
+++ client/src/components/student_components/drawer/student_drawer.js
@@ -10,11 +10,11 @@ const StudentDrawer = ({url}) => {
             <div className='drawer-container'>
                 <nav>
                     <ul className='link-list'>
-                        <li><NavLink activeStyle={{color:'#000000'}} className='link-to-section' to={`${url}/docs`}><i className='material-icons md-36'>text_snippet</i><div className='topic-container'>Recursos Didacticos</div></NavLink></li>
-                        <li><NavLink activeStyle={{color:'#000000'}} className='link-to-section' to={`${url}/subjects`}><i className='material-icons md-36'>list</i><div className='topic-container'>Carga Academica</div></NavLink></li>
-                        <li><NavLink activeStyle={{color:'#000000'}} className='link-to-section' to={`${url}/pick_subjects`}><i className='material-icons md-36'>assignment_turned_in</i><div className='topic-container'>Seleccion de Materias</div></NavLink></li>
-                        <li><NavLink activeStyle={{color:'#000000'}} className='link-to-section' to={`${url}/school_grades`}><i className='material-icons md-36'>history_edu</i><div className='topic-container'>Calificaciones</div></NavLink></li>
-                        <li><NavLink activeStyle={{color:'#000000'}} className='link-to-section' to={`${url}/school_schedule`}><i className='material-icons md-36'>table_chart</i><div className='topic-container'>Horario Escolar</div></NavLink></li>
+                        <li><NavLink activeStyle={{color:'#000000', background:'#4f9a94'}} className='link-to-section' to={`${url}/docs`}><i className='material-icons md-24'>text_snippet</i><div className='topic-container'>Recursos Didacticos</div></NavLink></li>
+                        <li><NavLink activeStyle={{color:'#000000', background:'#4f9a94'}} className='link-to-section' to={`${url}/subjects`}><i className='material-icons md-24'>list</i><div className='topic-container'>Carga Academica</div></NavLink></li>
+                        <li><NavLink activeStyle={{color:'#000000', background:'#4f9a94'}} className='link-to-section' to={`${url}/pick_subjects`}><i className='material-icons md-24'>assignment_turned_in</i><div className='topic-container'>Seleccion de Materias</div></NavLink></li>
+                        <li><NavLink activeStyle={{color:'#000000', background:'#4f9a94'}} className='link-to-section' to={`${url}/school_grades`}><i className='material-icons md-24'>history_edu</i><div className='topic-container'>Calificaciones</div></NavLink></li>
+                        <li><NavLink activeStyle={{color:'#000000', background:'#4f9a94'}} className='link-to-section' to={`${url}/school_schedule`}><i className='material-icons md-24'>table_chart</i><div className='topic-container'>Horario Escolar</div></NavLink></li>
                     </ul>
                 </nav>
             </div>
