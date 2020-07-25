175f0aa264682b74138f8bdfce2f00210a071837
Ramiro Angel Pantoja Cordoba <worddraco1@gmail.com>
2020-07-24 22:01:45 -0500

    Ajuste de UX en el componente Dropdown, no muy importante.

--- client/src/components/dropdown_user.js
+++ client/src/components/dropdown_user.js
@@ -4,19 +4,22 @@ import DropdownContent from './student_components/dropdown_content';
 import { useHistory } from 'react-router-dom';
 
 const DropdownUser = ({sessionEntity, handleLogOutStudent}) => {
-    let history = useHistory();
     const { getUserStudentAuth } = sessionEntity;
     const [ dropdown, setDropdown ] = useState(false);
+    const [ backgroundButton, setBackgroundButton ] = useState(false);
     
     const isUserAuth = getUserStudentAuth ? getUserStudentAuth.email : <span>NoUser</span>
     const isDropdown = dropdown ? <DropdownContent handleLogOutStudent={handleLogOutStudent}/> : null;
+    const isBackGroundButton = backgroundButton ? '#80cbc4' : '';
 
     const handleOnClickDropDown = (e) => {
         e.preventDefault();
-        if (dropdown === true) {
-            setDropdown(false)
-        } else {
+        if (dropdown === false) {
+            setBackgroundButton(true)
             setDropdown(true)
+        } else {
+            setBackgroundButton(false)
+            setDropdown(false)
         }
     }
 
@@ -25,13 +28,14 @@ const DropdownUser = ({sessionEntity, handleLogOutStudent}) => {
             let clickOutside = e.target;
             if (clickOutside && dropdown === true) {
                 setDropdown(false)
+                setBackgroundButton(false)
             }
         }
     });
 
     return (
         <div className='dropdown-container'>
-            <button type='button'className='dropdown-button' onClick={(e) => {handleOnClickDropDown(e)}}>
+            <button type='button'className='dropdown-button' style={{background:isBackGroundButton}} onClick={(e) => {handleOnClickDropDown(e)}}>
                 <div className='usericon-dropdown'>
                     <i className='material-icons md-24'>account_circle</i>
                 </div>
